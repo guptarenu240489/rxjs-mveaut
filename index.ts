420f204d8d74b42495f70441a82dac31a3a2c105
guptarenu240489 <guptarenu24@gmail.com>
2022-08-23 12:29:45 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -13,19 +13,25 @@ const observable$ = new Observable((subsciber) => {
   subsciber.next('Ashish');
   setTimeout(() => subsciber.next('Renu'), 2000);
   subsciber.next('Riya');
-  // subsciber.error('Error occusred');
+  setTimeout(() => subsciber.error(new Error('Error occured')), 4000);
   // will not execute as after error occur, observableis complete and destroyed
   // subsciber.next('Not sure');
+  // subsciber.complete();
+  // teardown can be run after complete or error
+  return () => {
+    console.log('Teardown');
+  };
 });
 
 const observer = {
   next: (val) => console.log(val),
-  error: (err) => console.log(err),
+  error: (err) => console.log(err.message),
+  complete: () => console.log('Completed'),
 };
 
 observable$.subscribe(observer);
 // will subscribe again and duplicate result will come
-setTimeout(() => {
-  console.log('observable 2 executed');
-  observable$.subscribe(observer);
-}, 1000);
+// setTimeout(() => {
+//   console.log('observable 2 executed');
+//   observable$.subscribe(observer);
+// }, 1000);
