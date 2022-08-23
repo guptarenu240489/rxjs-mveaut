40dd166782aa60e7b6fd9c4b797168aac743823d
guptarenu240489 <guptarenu24@gmail.com>
2022-08-23 11:45:09 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -7,3 +7,22 @@ of('World')
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
+
+const observable$ = new Observable((subsciber) => {
+  console.log('Observable executed');
+  subsciber.next('Ashish');
+  subsciber.next('Renu');
+  subsciber.next('Riya');
+  // subsciber.error('Error occusred');
+  // will not execute as after error occur, observableis complete and destroyed
+  // subsciber.next('Not sure');
+});
+
+const observer = {
+  next: (val) => console.log(val),
+  error: (err) => console.log(err),
+};
+
+observable$.subscribe(observer);
+// will subscribe again and duplicate result will come
+observable$.subscribe(observer);
