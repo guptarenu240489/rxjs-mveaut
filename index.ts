cd580777eadd842580b7a53410c2a010c7b0b1ab
guptarenu240489 <guptarenu24@gmail.com>
2022-08-23 12:35:38 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -2,36 +2,14 @@ import './style.css';
 
 import { of, map, Observable } from 'rxjs';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
-  .subscribe(console.log);
-
-// Open the console in the bottom right to see results.
-
-const observable$ = new Observable((subsciber) => {
-  console.log('Observable executed');
-  subsciber.next('Ashish');
-  setTimeout(() => subsciber.next('Renu'), 2000);
-  subsciber.next('Riya');
-  setTimeout(() => subsciber.error(new Error('Error occured')), 4000);
-  // will not execute as after error occur, observableis complete and destroyed
-  // subsciber.next('Not sure');
-  // subsciber.complete();
-  // teardown can be run after complete or error
-  return () => {
-    console.log('Teardown');
-  };
+const observable$ = new Observable<number>((subscriber) => {
+  let count = 1;
+  setInterval(() => {
+    subscriber.next(count++);
+  }, 1000);
 });
 
-const observer = {
-  next: (val) => console.log(val),
-  error: (err) => console.log(err.message),
-  complete: () => console.log('Completed'),
-};
-
-observable$.subscribe(observer);
-// will subscribe again and duplicate result will come
-// setTimeout(() => {
-//   console.log('observable 2 executed');
-//   observable$.subscribe(observer);
-// }, 1000);
+const subscription = observable$.subscribe((val) => console.log(val));
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 7000);
