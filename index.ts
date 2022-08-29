29301b6a346771b3b7e83ff22614259198e1f161
guptarenu240489 <guptarenu24@gmail.com>
2022-08-29 18:21:50 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,33 +1,24 @@
-import { catchError, filter, Observable, of } from 'rxjs';
+import { EMPTY, fromEvent, of, switchMap } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { catchError, concatMap, map } from 'rxjs/operators';
 
-interface NewsItem {
-  category: 'Business' | 'Sport';
-  content: string;
-}
-
-const newsFeed$ = new Observable<NewsItem>((subscriber) => {
-  setTimeout(
-    () => subscriber.next({ category: 'Business', content: 'A' }),
-    1000
-  );
-  setTimeout(() => subscriber.next({ category: 'Sport', content: 'B' }), 3000);
-  setTimeout(
-    () => subscriber.next({ category: 'Business', content: 'C' }),
-    4000
-  );
-  setTimeout(() => subscriber.next({ category: 'Sport', content: 'D' }), 6000);
-  setTimeout(
-    () => subscriber.next({ category: 'Business', content: 'E' }),
-    7000
-  );
-  setTimeout(() => subscriber.error('Error Ocurrred'), 8000);
-});
-// newsFeed$.subscribe((item) => console.log(item));
-
-newsFeed$
+const endpointInput: HTMLInputElement =
+  document.querySelector('input#endpoint');
+const fetchButton = document.querySelector('button#fetch');
+//
+// concatMap will emit outer observable notifications and  then it will complete inner observable. once inner observable is complete then it will emit further outer observable notifications. It will wueue outer observale notification unless inner observable for preious is complete
+// If another outer observable starts emmit
+fromEvent(fetchButton, 'click')
   .pipe(
-    filter((item) => item.category === 'Business'),
-    // rather then throwings error, we handles it and emit new notification which is not of type error
-    catchError((err) => of('SOme error occured'))
+    map(() => endpointInput.value),
+    switchMap((value) =>
+      ajax(`https://random-data-api.com/api/v2/${value}`).pipe(
+        catchError(() => of('Error occured!!'))
+      )
+    )
   )
-  .subscribe((item) => console.log(item));
+  .subscribe({
+    next: (value) => console.log(value),
+    error: (err) => console.log('Error:', err),
+    complete: () => console.log('Completed'),
+  });
